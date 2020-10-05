e90bedcd87c1a40ab447b00bc8c6a2d6c13df361
xerosanyam <xerosanyam@gmail.com>
2020-10-05 12:47:12 +0530

    init

---
+++ src/test/java/com/example/razorpay/RazorpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.razorpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RazorpayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
